cc74364048d0b5b4e51cbcc4a90980d72ee3cd92
tweezers-fmf <tweezers.fmf@gmail.com>
2020-06-03 10:48:11 +0200

    Added functionality for optical tweezers control

---
+++ tweezers/conf.py
@@ -0,0 +1,35 @@
+# Logging level, set to 'DEBUG' or 'INFO' to display messages in console. For debugging mainly...
+LOGLEVEL = 'WARN'
+
+ERRORLIST = {
+    '0':  "no error",
+    '1':  "unknown command",
+    '2':  "command active",
+    '3':  "maximum number of force measurement traps exceeded",
+    '4':  "manipulation traps not available under current license",
+    '5':  "maximum number of manipulation traps exceeded",
+    '6':  "trap out of working range",
+    '7':  "invalid trap number",
+    '8':  "invalid trap strength",
+    '9':  "invalid trap position",
+    '10': "maximum number of sequences exceeded",
+    '11': "invalid sequence number",
+    '12': "sequence assigned to trap - can't delete",
+    '13': "invalid seq. rotation/camera parameter invalid",
+    '14': "invalid seq. scale/camera parameter not set "
+          "(no camera or invalid parameter)",
+    '15': "invalid file name",
+    '16': "invalid recording/snapshot type",
+    '17': "error accessing video stream",
+    '18': "recoding not possible",
+    '19': "not possible to stop recording",
+    '20': "invalid beam focus parameter",
+    '21': "can not set beam focus",
+    '22': "invalid laser level parameter",
+    '23': "can not set laser level",
+    '30': "TSF missing",
+    '31': "TSF invalid data",
+    '32': "TSF read/write error",
+    '33': "TSF too long",
+    '34': "TSF empty"
+}
---
+++ tweezers/tweezersTCP.py
@@ -0,0 +1,77 @@
+import socket
+from .conf import ERRORLIST, LOGLEVEL
+from labtools.log import create_logger
+
+logger = create_logger(__name__, LOGLEVEL)
+
+class Optical:
+
+    # default gateways
+    HOST = '127.0.0.1'
+    PORT = 2070
+
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((Optical.HOST, Optical.PORT))
+        logger.info(f'Connecting to {Optical.HOST}:{Optical.PORT}')
+
+        # test connection
+        connTest = self._query('test')
+        if not connTest == 'unknown command':
+            logger.warn(f'Got {connTest} instead of "unknown command".')
+
+    def _query(self, order=''):
+        logger.debug(f'Sending command "{order}".')
+        self.s.send(f'{order}\r\n'.encode())
+
+        echo = self.s.recv(1024).decode().strip()
+        logger.debug(f'Received error: "{echo}" - {ERRORLIST[echo]}')
+        return ERRORLIST[echo]
+
+    def setRecFile(self, fileName=''):
+        self._query(f'CAMERA_SET_REC_FILE {fileName}')
+
+    def setImgFile(self, fileName=''):
+        self._query(f'CAMERA_SET_IMG_FILE {fileName}')
+
+    def startRecording(self, FMmode=True):
+        fm = '_FM' if FMmode else ''
+        self._query(f'CAMERA_REC_START{fm}')
+
+    def stopRecording(self):
+        self._query('CAMERA_REC_STOP')
+
+    def takeImage(self):
+        self._query('CAMERA_IMG_SINGLE')
+
+    def setClock(self, clock_MHz):
+        """ Set camera pixel clock in MHz
+        """
+        self._query(f'CAMERA_SET_CLOCK {clock_MHz}')
+
+    def setFPS(self, fps=10):
+        self._query(f'CAMERA_SET_FRAME_RATE {fps}')
+
+    def setExposure(self, exposure_ms=200):
+        self._query(f'CAMERA_SET_EXPOSURE {exposure_ms}')
+
+    def setGain(self, gain=1):
+        self._query(f'CAMERA_SET_GAIN {gain}')
+
+    def focus(self, beamFocus=500):
+        self._query(f'BEAM_SET_FOCUS {beamFocus}')
+
+    def laserOn(self):
+        self._query(f'LASER_ON')
+
+    def laserOff(self):
+        self._query(f'LASER_OFF')
+
+    def laserLevel(self, level=0.2):
+        self._query(f'LASER_SET_LEVEL {level}')
+
+    # TODO: add orders from CLEAR_PROJECT on
+
+if __name__ == '__main__':
+
+    test = Optical()
