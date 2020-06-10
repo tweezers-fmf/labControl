ba08b2fd119ae3a8f8c6c47937f4954bfad08f65
tweezers-fmf <tweezers.fmf@gmail.com>
2020-06-10 09:55:53 +0200

    Changed MagTweez and Optical tweezers. Server restarts for every query. Forwarded for testing ...

--- tweezers/tweezersTCP.py
+++ tweezers/tweezersTCP.py
@@ -4,29 +4,37 @@ from labtools.log import create_logger
 
 logger = create_logger(__name__, LOGLEVEL)
 
-class Optical:
 
-    # default gateways
-    HOST = '127.0.0.1'
-    PORT = 2070
+class Tweezer:
 
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((Optical.HOST, Optical.PORT))
-        logger.info(f'Connecting to {Optical.HOST}:{Optical.PORT}')
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        pass
+
+    def _query(self, order='', bufferSize=1024):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            logger.debug(f'Sending command "{order}".')
+            s.send(f'{order}\r\n'.encode())
+
+            echo = s.recv(bufferSize).decode().strip()
+            logger.debug(f'Received error: "{echo}"')
+        return echo
+
+
+class Optical(Tweezer):
+
+    def __init__(self, host='88.200.77.138', port=2070):
+        super().__init__(host, port)
 
         # test connection
         connTest = self._query('test')
-        if not connTest == 'unknown command':
-            logger.warn(f'Got {connTest} instead of "unknown command".')
+        if not connTest == '1':
+            logger.warn(f'Got {connTest} instead of "1".')
 
-    def _query(self, order=''):
-        logger.debug(f'Sending command "{order}".')
-        self.s.send(f'{order}\r\n'.encode())
-
-        echo = self.s.recv(1024).decode().strip()
-        logger.debug(f'Received error: "{echo}" - {ERRORLIST[echo]}')
-        return ERRORLIST[echo]
+    def query(self, order='', bufferSize=1024):
+        return ERRORLIST(self._query(order, bufferSize))
 
     def setRecFile(self, fileName=''):
         return self._query(f'CAMERA_SET_REC_FILE {fileName}')
@@ -72,37 +80,95 @@ class Optical:
 
     # TODO: add orders from CLEAR_PROJECT on
 
-class Magnetic:
+class Magnetic(Tweezer):
+    DIR = {
+        'x':(0, 1),
+        'y':(6, 7),
+        'z':(4, 5)
+    }
 
-    # default gateway
-    HOST = '88.200.78.109'
-    PORT = 2222
-
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((Magnetic.HOST, Magnetic.PORT))
-        logger.info(f'Connecting to {Magnetic.HOST}:{Magnetic.PORT}')
+    def __init__(self, host='88.200.78.109', port=2222):
+        super().__init__(host, port)
 
         # test connection
-        connTest = self._query('test')
-        print(connTest)
-        # if not connTest == 'unknown command':
-        #     logger.warn(f'Got {connTest} instead of "unknown command".')
+        connTest = self.query('test')
+        logger.info(f'Tested connection on {self.host}:{self.port}. Response = {connTest}')
 
-    def _query(self, order=''):
-        logger.debug(f'Sending command "{order}".')
-        self.s.send(f'{order}\r\n'.encode())
+    def query(self, order='', bufferSize=1024):
+        return self._query(order, bufferSize)
 
-        echo = self.s.recv(1024).decode().strip()
-        # logger.debug(f'Received error: "{echo}" - {ERRORLIST[echo]}')
-        # return ERRORLIST[echo]
-        return echo
+    def synchronize(self):
+        return self.query('synchronize')
 
-    def setDC(self):
-        return self._query(f'setdc 0')
+    def reset(self):
+        return self.query('reset')
+
+    def disable(self, direction='x'):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'disable {dirs[0]}')
+        r2 = self.query(f'disable {dirs[1]}')
+
+        return r1, r2
+
+    def setDC(self, direction='x'):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'setdc {dirs[0]}')
+        r2 = self.query(f'setdc {dirs[1]}')
+
+        return r1, r2
+
+    def setDCAmplitude(self, direction='x', amplitude=0.):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'dc_value {dirs[0]} {amplitude}')
+        r2 = self.query(f'dc_value {dirs[1]} {amplitude}')
+
+        return r1, r2
+
+    def setWaveform(self, direction='x'):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'waveform {dirs[0]}')
+        r2 = self.query(f'waveform {dirs[1]}')
+
+        return r1, r2
+
+    def setFrequency(self, direction='x', frequency=0.):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'setfreq {dirs[0]} {frequency}')
+        r2 = self.query(f'setfreq {dirs[1]} {frequency}')
+
+        return r1, r2
+
+    def setAmplitude(self, direction='x', amplitude=0.):
+        dirs = self.DIR[direction]
+
+        r1 = self.query(f'scale {dirs[0]} {amplitude}')
+        r2 = self.query(f'scale {dirs[1]} {amplitude}')
+
+        return r1, r2
+
+    def setPhase(self, direction='x', phase=0., shift=180.):
+        dirs = self.DIR[direction]
+        if phase+shift > 360:
+            shift -= 360
+
+        r1 = self.query(f'scale {dirs[0]} {phase}')
+        r2 = self.query(f'scale {dirs[1]} {phase+shift}')
+
+        return r1, r2
+
+
 
 if __name__ == '__main__':
+    from labControl import timing
 
     test = Magnetic()
+
+    timing.sleep(2)
+
     test.setDC()
 
